63855e8bbc3bf15dfdefbafe7005da395f469832
Diego Rocha <dhsrocha@gmail.com>
2018-01-08 02:03:11 -0200

    Correção sobre o perfil de construção para o TDD.
    Foi verificado que a vigilância das tarefas eram interrompidas
    quando ocorria algum tipo de falha. Portanto, foi incluído o plugin
    'gulp-plumber' para resolver o caso.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+'use strict';
 const g = require('gulp'), $ = require('gulp-load-plugins')
 /* <-- switcher for debugging
 ({DEBUG:true});/*/({DEBUG:false});//*/
@@ -6,21 +7,28 @@ const def = 'default', lint = 'jslint', test = 'test', tdd = 'tdd';
 
 const seq = [lint,test,def];
 
+function errorHandler(err) {
+  console.log(err);
+  this.emit('end');
+}
+
 g.task(def, seq);
 
-g.watch(['**/*.js'], [lint]);
-
-g.task(tdd, () => 
-  g.start(lint, test)
-   .watch(['masker.js', 'test.mocha.js'], [test]));
+g.task(tdd, () => {
+  g.start(lint, test);
+  g.watch(['!node_modules','!node_modules/**','./*.js'], [lint]);
+  g.watch(['masker.js', 'test.mocha.js'], [test]);
+});
 
 g.task(lint, () =>
   g.src(['!node_modules','!node_modules/**','./masker.js'])
+   .pipe($.plumber({errorHandler: errorHandler}))
    .pipe($.jslint())
 );
 
-g.task(test, [lint], () =>
+g.task(test, () =>
   g.src('test.mocha.js', {read: false})
+   .pipe($.plumber({errorHandler: errorHandler}))
    .pipe($.mocha())
    .pipe($.istanbul.writeReports())
 );
